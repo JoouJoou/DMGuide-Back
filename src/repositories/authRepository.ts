f76268ac165f09ad428892fc21ac4c996dda620f
JoouJoou <rogerio.junior1016@gmail.com>
2023-02-01 19:16:41 -0300

    Feat: Create authRepository

---
+++ src/repositories/authRepository.ts
@@ -0,0 +1,6 @@
+import { prisma } from "../config/database";
+import * as authTypes from "../types/authTypes";
+
+export async function insertUser(userData: authTypes.IUserData) {
+  return "picas";
+}
