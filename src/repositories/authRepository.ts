63ed72a6efb251a3925085e0294c9027043708b7
JoouJoou <rogerio.junior1016@gmail.com>
2023-02-05 20:09:54 -0300

    Feat: Auth Repository

--- src/repositories/authRepository.ts
+++ src/repositories/authRepository.ts
@@ -1,6 +1,15 @@
 import { prisma } from "../config/database";
+import { Prisma } from "@prisma/client";
 import * as authTypes from "../types/authTypes";
 
-export async function insertUser(userData: authTypes.IUserData) {
-  return "picas";
+export async function insertUser(userData: authTypes.IUserData, userId = 0) {
+  return await prisma.user.create({
+    select: {
+      email: true,
+      name: true,
+      picture: true,
+      password: true,
+    },
+    data: userData,
+  });
 }
