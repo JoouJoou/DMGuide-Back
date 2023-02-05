c9895760daf4bc56422254253e93d878a19e94dc
JoouJoou <rogerio.junior1016@gmail.com>
2023-02-05 20:08:27 -0300

    Feat: Auth done 2

--- src/routes/authRouter.ts
+++ src/routes/authRouter.ts
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import * as authController from "../controllers/authController";
 import { validateSchema } from "../middlewares/validations/schemaValidator";
-import { signupSchema } from "../schemas/authSchema";
+import { signinSchema, signupSchema } from "../schemas/authSchema";
 
 const authRouter = Router();
 
 authRouter.post("/signup", validateSchema(signupSchema), authController.signUp);
+authRouter.post("/signin", validateSchema(signinSchema), authController.signIn);
 
 export default authRouter;
